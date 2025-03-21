052862569d6e2932dadc2c3042ffb5aecec88af5
EgzontDe <egzont.demiri112@gmail.com>
2025-03-21 19:28:55 +0100

    Frontend fix

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
