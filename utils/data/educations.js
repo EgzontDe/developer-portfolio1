f7739e83613dfaf9132b2dc0405ed6d5a032f383
EgzontDe <egzont.demiri112@gmail.com>
2025-03-21 15:33:49 +0100

    Frontend fix

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -4,20 +4,20 @@ export const educations = [
     title: "Master of Science in Computer Science",
     duration: "2022 - 2025",
     institution: "BeNeFri (Bern, Neuchatel, Fribourg",
-    Country: "Switzerland",
+    Country: "Bern, Switzerland",
   },
   {
     id: 2,
     title: "Bachelor of Science in Computer Science",
     duration: "2020 - 2022",
     institution: "University AAB",
-    Country: "Kosovo",
+    Country: "Prishtine, Kosovo",
   },
   {
     id: 3,
     title: "Qualified employe with high skills in IT",
     duration: "2008 - 2012",
     institution: "Andrea Durresaku",
-    Country: "Kosovo",
+    Country: "Kamenice, Kosovo",
   }
 ]
