585393812edeb820e1661880a79a96877abe0b3b
egzont <egzont.demiri112@gmail.com>
2025-05-31 20:40:38 +0200

    Fix project data

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: '/profile.jpeg',
   designation: "Software Engineer",
   description: "I’m Egzont Demiri, a Software Engineer with strong experience in software development, system infrastructure, and web technologies. I hold a Master’s degree in Computer Science from the BeNeFri Joint Program (Bern, Neuchâtel, Fribourg). Over the years, I’ve worked on diverse projects involving fullstack web development, blockchain integration, cloud deployments, and secure server setups. I’m passionate about building reliable, efficient, and user-focused solutions. I’m always eager to learn, explore new technologies, and take on challenging problems.",
-  email: 'egzont.demiri@gmail.com',
+  email: 'egzont.demiri@gmx.ch',
   phone: '+41 76 733 62 95',
   address: 'Burgdorf, Switzerland',
   github: 'https://github.com/EgzontDe',
