ac965976267cc35ffe39171e074b7258d84cd351
egzont <egzont.demiri112@gmail.com>
2025-09-05 15:08:06 +0200

    Fix project data

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,21 +2,21 @@ export const educations = [
   {
     id: 1,
     title: "Master of Science in Computer Science",
-    duration: "2022 - 2025",
+    duration: "Completed",
     institution: "BeNeFri Bern, Neuchatel, Fribourg",
     Country: "Bern, Switzerland",
   },
   {
     id: 2,
     title: "Bachelor of Science in Computer Science",
-    duration: "2020 - 2022",
+    duration: "Completed",
     institution: "University AAB",
     Country: "Prishtine, Kosovo",
   },
   {
     id: 3,
     title: "Qualified employee with high skills in IT",
-    duration: "2008 - 2012",
+    duration: "Completed",
     institution: "Andrea Durresaku",
     Country: "Kamenice, Kosovo",
   }
